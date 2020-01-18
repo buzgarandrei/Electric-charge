badae8cf19747ff00ea2a97e43e3666b767d9c12
Andrei_Buzgar <andyboy17.ab@gmail.com>
2020-01-19 01:17:14 +0200

    no message

--- src/main/java/com/example/demo/repositories/MockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import java.sql.SQLIntegrityConstraintViolationException;
-
-public interface MockRepository {
-    void populateDB() throws SQLIntegrityConstraintViolationException;
-}
